d7ecaa448f0e3fa2cd2fd472ba916c59941f8e04
Baram <chcbaram@gmail.com>
2021-01-01 20:00:50 +0900

    UART 통신 추가.

--- stm32cli/src/ap/ap.c
+++ stm32cli/src/ap/ap.c
@@ -22,15 +22,59 @@ void apInit(void)
 
 void apMain(int argc, char *argv[])
 {
+  uint8_t  uart_ch;
+  char    *uart_port;
+  uint32_t uart_baud;
+
+
+  if (argc != 3)
+  {
+    logPrintf("wrong args\n");
+    apExit();
+  }
+
+  uart_ch   = _DEF_UART2;
+  uart_port = argv[1];
+  uart_baud = (int32_t)strtoul(argv[2], (char **)NULL, (int) 0);
+
+  logPrintf("uart ch   : %d\n", uart_ch);
+  logPrintf("uart port : %s\n", uart_port);
+  logPrintf("uart baud : %d bps\n", uart_baud);
+
+  if (uartOpenPort(uart_ch, uart_port, uart_baud) == true)
+  {
+    logPrintf("uart open : OK\n");
+  }
+  else
+  {
+    logPrintf("uart open : Fail\n");
+    apExit();
+  }
+
 
   while(1)
   {
-    cliMain();
+    //cliMain();
+
+    if (uartAvailable(_DEF_UART1) > 0)
+    {
+      uartPrintf(_DEF_UART2, "%c", uartRead(_DEF_UART1));
+    }
+
+    if (uartAvailable(_DEF_UART2) > 0)
+    {
+      uartPrintf(_DEF_UART1, "%c", uartRead(_DEF_UART2));
+    }
   }
 }
 
 void apExit(void)
 {
-  printf("cli exit\n");
+  for (int i=0; i<UART_MAX_CH; i++)
+  {
+    uartClose(i);
+  }
+  printf("\ncli exit\n");
   exit(0);
 }
+
